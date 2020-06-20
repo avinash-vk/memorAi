32eb28211d018f02f50d62d4f972795bb0c3effc
avinash-vk <avinash2000vk@gmail.com>
2020-06-20 22:04:36 +0530

    better file structure

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/pages/Dashboard.dart';
-import 'package:frontend/pages/ImageReg.dart';
+import 'package:frontend/pages/registration/ImageReg.dart';
 import 'package:frontend/pages/Login.dart';
 import 'package:frontend/pages/Home.dart';
 import 'package:frontend/pages/Chat.dart';
-import 'package:frontend/pages/Signup.dart';
-import 'package:frontend/pages/locationReg.dart';
+import 'package:frontend/pages/registration/Signup.dart';
+import 'package:frontend/pages/registration/locationReg.dart';
 void main(){ 
   runApp(
   MaterialApp(
