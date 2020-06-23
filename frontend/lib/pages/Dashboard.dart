74da78e4e8829eaf6b0c6193a45bb4c74f15cd88
Praatibh Surana <43675042+praatibhsurana@users.noreply.github.com>
2020-06-23 23:44:42 +0530

    Add files via upload

---
+++ frontend/lib/pages/Dashboard.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/components/Appbar.dart';
+
+class Dashboard extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: CustomAppBar(),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage("https://images.unsplash.com/photo-1506794778202-cad84cf45f1d"),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.redAccent,
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.redAccent[400],
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Joe',
+              style: TextStyle(
+                color: Colors.black54,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'Contact no.',
+              style: TextStyle(
+                color: Colors.redAccent[400],
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '9999966777',
+              style: TextStyle(
+                color: Colors.black54,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'joe@medicallifestyle.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20,),
+            Text('qr code comes here if needed'),
+          ],
+        ),
+      ),
+    );
+  }
+}
